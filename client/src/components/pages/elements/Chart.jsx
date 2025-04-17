bb54b4ef09f5beb63ab54bf935585959f1cc57bb
Aaron Mull <aaronmull421@gmail.com>
2025-04-16 20:58:54 -0400

    Better looking charts

--- client/src/components/pages/elements/Chart.jsx
+++ client/src/components/pages/elements/Chart.jsx
@@ -12,35 +12,58 @@ function Chart({ data }) {
     const getAthleteData = () => {
         if(!selectedAthlete) return []
 
-        return data
+        const entriesByDate = {}
+
+        data
             .filter(entry => entry.athlete === selectedAthlete)
-            .map(entry => ({
-                date: new Date(entry.date).toLocaleDateString(),
-                time: entry.time
-            }))
-            .sort((a, b) => new Date(a.date) - new Date(b.date))
+            .forEach(entry => {
+                
+                const date = new Date(entry.date).toLocaleDateString('en-US', { timeZone: 'UTC' })
+
+                if(!entriesByDate[date] || entry.time < entriesByDate[date].time) {
+                    entriesByDate[date] = {
+                        date,
+                        time: Number(entry.time),
+                    }
+                }
+            })
+        
+            return Object.values(entriesByDate)
+                .sort((a, b) => new Date(a.date) - new Date(b.date))
+
     }
 
     useEffect(() => {
         setAthleteData(getAthleteData())
     }, [selectedAthlete, data])
 
+
+    const CustomTooltip = ({ active, payload, label }) => {
+        if( active && payload && payload.length ) {
+            return (
+                <div className="tooltip">
+                    <p>{label}</p>
+                    <p>Time:
+                        <span> {payload[0].value}</span>
+                    </p>
+                </div>
+            )
+        }
+
+    }
+
     return(
         <div className="chart">
             <label htmlFor="athlete">Select Athlete: </label>
             <AthleteDropdown selectedAthlete={selectedAthlete} setSelectedAthlete={setSelectedAthlete} />
-            
             {selectedAthlete && athleteData.length > 0 ? (
                 <ResponsiveContainer width="100%" height={300}>
-                    <LineChart 
-                        data={athleteData}
-                        width={500}
-                    >
-                        <CartesianGrid strokeDasharray="3 3" />
+                    <LineChart data={athleteData} width={750}>
+                        <CartesianGrid strokeDasharray="2 2" />
                         <XAxis dataKey="date" />
-                        <YAxis />
-                        <Tooltip />
-                        <Line type="monotone" dataKey="time" stroke="#8884d8" />
+                        <YAxis dataKey="time" domain={['auto', 'auto']} />
+                        <Tooltip content={<CustomTooltip />} />
+                        <Line type="monotone" dataKey="time" stroke="#3b82f6" />
                     </LineChart>
                 </ResponsiveContainer>
             ) : (
